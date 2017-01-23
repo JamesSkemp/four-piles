423d25f3a9f892c101a091382056d9f114f803a8
James skemp <strivinglife@gmail.com>
2017-01-22 20:44:09 -0600

    Add functionality to easily get the key name of a card

--- src/Prefabs/Card.ts
+++ src/Prefabs/Card.ts
@@ -8,6 +8,10 @@ module GameIdea45Project {
 			this.suit = suit;
 		}
 
+		getCardKeyName(): string {
+			return 'card' + Suit[this.suit] + this.rank;
+		}
+
 		toString() : string {
 			return Rank[this.rank] + " of " + Suit[this.suit];
 		}
