9bff240fbd43e582d2881fef0a84d6d1c8433d41
James skemp <strivinglife@gmail.com>
2017-01-22 22:37:47 -0600

    Add new prefab for card piles

---
+++ src/Prefabs/CardPile.ts
@@ -0,0 +1,39 @@
+module GameIdea45Project {
+	export class CardPile extends Phaser.Sprite {
+		cards: Card[];
+		showTopCard: boolean;
+		hideIfEmpty: boolean;
+		setupTween: Phaser.Tween;
+
+		constructor(game: Phaser.Game, x: number, y: number, deckBackId: number = 0, showTopCard: boolean = true, hideIfEmpty: boolean = true) {
+			super(game, x, y, 'cardBacks', deckBackId);
+
+			this.showTopCard = showTopCard;
+			this.hideIfEmpty = hideIfEmpty;
+
+			this.cards = [];
+			if (this.hideIfEmpty) {
+				this.visible = false;
+			}
+
+			this.anchor.set(0.5);
+			this.inputEnabled = true;
+
+			game.add.existing(this);
+		}
+
+		addCard(card: Card): void {
+			this.cards.push(card);
+
+			if (!this.visible) {
+				this.visible = true;
+			}
+
+			if (this.showTopCard) {
+				this.loadTexture(card.getCardKeyName());
+			}
+			console.log('New cards: ' + this.cards);
+			console.log(this);
+		}
+	}
+}
