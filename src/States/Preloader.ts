036e81e3c1983039680f76ea22eb75a70509e3db
James skemp <strivinglife@gmail.com>
2017-01-22 19:29:05 -0600

    Add functionality to preload all individual cards

--- src/States/Preloader.ts
+++ src/States/Preloader.ts
@@ -11,6 +11,13 @@
 			// Assets loaded here can include image and audio files, as well as sprite sheets and more.
 			this.load.spritesheet('cardBacks', 'playingCardBacks.png', 140, 190, 15);
 			this.load.image('cardJoker');
+			var suits = [Suit.Spades, Suit.Hearts, Suit.Clubs, Suit.Diamonds];
+			var ranks = [Rank.Ace, Rank.Two, Rank.Three, Rank.Four, Rank.Five, Rank.Six, Rank.Seven, Rank.Eight, Rank.Nine, Rank.Ten, Rank.Jack, Rank.Queen, Rank.King];
+			suits.forEach((suit) => 
+				ranks.forEach((rank) => 
+					this.load.image('card' + Suit[suit] + rank + '')
+				)
+			);
 		}
 
 		create() {
