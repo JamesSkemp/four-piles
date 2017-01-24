53803f146d5fb880df3724262b7ff9637d891dd5
James skemp <strivinglife@gmail.com>
2017-01-24 10:42:12 -0600

    Update main menu to display card to click to start

--- src/States/MainMenu.ts
+++ src/States/MainMenu.ts
@@ -1,10 +1,31 @@
 ﻿module GameIdea45Project {
 	export class MainMenu extends Phaser.State {
+		mainDeck: Phaser.Sprite;
+		mainDeckTween: Phaser.Tween;
+
+		deckBackId: number = 10;
+
 		create() {
 			console.log((new Date).toISOString() + ' : Entered MainMenu create()');
 
 			// Handle user input as needed.
-			this.game.state.start('MainGame');
+			this.mainDeck = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'cardBacks', this.deckBackId);
+			this.mainDeck.anchor.set(0.5);
+			this.mainDeck.inputEnabled = true;
+
+			this.mainDeckTween = this.game.add.tween(this.mainDeck);
+			this.mainDeckTween.to({
+				y: this.game.world.height + this.mainDeck.height
+			}, 1500, Phaser.Easing.Linear.None);
+			this.mainDeckTween.onComplete.add(() => {
+				this.game.state.start('MainGame');
+			});
+			
+			this.mainDeck.events.onInputDown.add(this.startGame, this);
+		}
+
+		startGame() {
+			this.mainDeckTween.start();
 		}
 	}
 }
