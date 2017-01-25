b60c41e56d86009201129480e0eaf5d8e3f9efcc
James skemp <strivinglife@gmail.com>
2017-01-25 17:59:04 -0600

    Add title, help text, and developer info to main menu

--- src/States/MainMenu.ts
+++ src/States/MainMenu.ts
@@ -2,6 +2,9 @@
 	export class MainMenu extends Phaser.State {
 		mainDeck: Phaser.Sprite;
 		mainDeckTween: Phaser.Tween;
+		mainTitle: Phaser.Text;
+		helpText: Phaser.Text;
+		subText: Phaser.Text;
 
 		create() {
 			console.log((new Date).toISOString() + ' : Entered MainMenu create()');
@@ -20,6 +23,20 @@
 			});
 			
 			this.mainDeck.events.onInputDown.add(this.startGame, this);
+
+			var baseTextStyle = { font: 'Arial', fontSize: '80px', fill: '#fff' };
+			// Add main title.
+			this.mainTitle = this.game.add.text(this.game.world.centerX, this.game.world.centerY / 3, "Four Piles", baseTextStyle);
+			this.mainTitle.x -= this.mainTitle.width / 2;
+			this.mainTitle.y -= this.mainTitle.height / 2;
+			// Add help text.
+			this.helpText = this.game.add.text(this.game.world.centerX, this.game.world.centerY * 1.5, "Click the Deck to Begin", baseTextStyle);
+			this.helpText.fontSize = '30px';
+			this.helpText.x -= this.helpText.width / 2;
+			this.helpText.y -= this.helpText.height / 2;
+			// Add developer name information.
+			this.subText = this.game.add.text(this.game.world.width / 10, this.game.world.centerY * 1.75, 'Developed by James Skemp, jamesrskemp.com', baseTextStyle);
+			this.subText.fontSize = '20px';
 		}
 
 		startGame() {
